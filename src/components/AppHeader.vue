6fda09abc534792d37a8a45d3ae943390b9587eb
Lam Yen Khoa <lamyenkhoa2003@gmail.com>
2026-08-22 11:00:43 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <router-link to="/" class="navbar-brand">
+      Ứng dụng Quản lý Danh bạ
+    </router-link>
+
+    <div class="navbar-nav mr-auto">
+      <router-link to="/" class="nav-link"> Danh bạ </router-link>
+    </div>
+  </nav>
+</template>
